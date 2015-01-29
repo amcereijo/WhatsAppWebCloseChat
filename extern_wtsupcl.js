66ba42eb797340697a684f4a8058b181c8f28076
angel_cereijo_medianet <angel.cereijo@medianet.es>
2015-01-29 09:01:56 +0100

    fixed to allow open the last closed chat view

--- extern_wtsupcl.js
+++ extern_wtsupcl.js
@@ -6,7 +6,7 @@ var ExternFunction = (function() {
 
 		onDocumentReady = function() {
 			var doc = $(document);
-			closeChatButton.on('click', onCloseChat);	
+			closeChatButton.on('click', onCloseChat);
 			//add listener to add close button when add a new chat window
 			insertionQ('div.pane-chat').summary(function(el) {
 				var menus = el[0].querySelectorAll('.menu-horizontal'),
@@ -14,21 +14,31 @@ var ExternFunction = (function() {
 				$(menus[correctMenu]).append(closeChatButton);
 			});
 			doc.off('click', '.chat', onDocumentReady);
+			doc.on('click', '.chat', onChatClick);
 			doc.off('ready', onDocumentReady);
 		},
 		onCloseChat = function() {
-			document.querySelector('.pane-chat').style.display = 'none';
+			getPaneChat().style.display = 'none';
+		},
+		onChatClick = function() {
+			var paneChat = getPaneChat();
+			if(paneChat && paneChat.style.display === 'none') {
+				paneChat.style.display = 'block' ;
+			}
+		},
+		getPaneChat = function () {
+			return document.querySelector('.pane-chat');
 		};
 
 	//public methods
 	return {
 		initPlugin : function () {
 			var doc = $(document);
-			//two events, the one which ocurrs first rules!! 
+			//two events, the one which ocurrs first rules!!
 			doc.on('ready', onDocumentReady);
 			doc.on('click', '.chat', onDocumentReady);
 		}
-	}
+	};
 })();
 
 //execute when load
